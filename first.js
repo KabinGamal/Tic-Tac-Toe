45a4603a13de38fdd3498ed72359086e03b8e838
KabinGamal <nibak1223@gmail.com>
2026-04-04 23:24:22 +0545

    condt

---
+++ first.js
@@ -0,0 +1,17 @@
+let button = document.querySelectorAll(".box");
+let msg = document.querySelector(".msg");
+let msg_cont = document.querySelector(".msg-cont");
+let turnO =true;
+
+
+const winConditions=[
+    [0,1,2],
+    [0,3,6],
+    [0,4,8],
+    [1,4,7],
+    [2,5,8],
+    [2,4,6],
+    [3,4,5],
+    [6,7,8],
+];
+
